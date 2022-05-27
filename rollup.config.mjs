1187dce53188920fd7ef256c347fa46fbe103df3
Luis Ball <lball@imgix.com>
2022-05-27 10:20:43 -0700

    feat: provide minified & esm exports
    Before this commit:
    
    We only built one `index.js` file that was esm syntax and un-minified.
    
    After this commit:
    
    We build two dirs with `ix-video.js`, `mini` and `esm`. Users can then
    choose if they need the minified bundle or not.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -11,29 +11,74 @@ import process from 'process';
 import postcss from 'rollup-plugin-postcss';
 import postcssLit from 'rollup-plugin-postcss-lit';
 import summary from 'rollup-plugin-summary';
+import {terser} from 'rollup-plugin-terser';
 import ts from 'rollup-plugin-ts';
 
 const devMode = process.env.NODE_ENV === 'development';
-console.log(`${devMode ? 'development' : 'production'} mode bundle`);
+const vjsBundleType = process.env.VJS_BUNDLE_TYPE || 'esm';
 
-export default {
-  input: './src/index.ts',
-  output: {
-    file: './dist/index.bundled.js',
-    format: 'esm',
+const buildConfig = {
+  minified: {
+    // minified bundle
+    input: './src/elements/ix-video.ts',
+    output: {
+      dir: './dist/min/',
+      format: 'esm',
+    },
+    onwarn(warning) {
+      if (warning.code !== 'THIS_IS_UNDEFINED') {
+        console.error(`(!) ${warning.message}`);
+      }
+    },
+    plugins: [
+      ts(),
+      replace({
+        'Reflect.decorate': 'undefined',
+        'process.env.VJS_BUNDLE_TYPE': JSON.stringify(vjsBundleType),
+        preventAssignment: true,
+      }),
+      resolve(),
+      postcss(),
+      postcssLit(),
+      // Minify JS
+      terser({
+        ecma: 2020,
+        module: true,
+        warnings: true,
+      }),
+      commonjs(),
+      summary(),
+    ],
   },
-  onwarn(warning) {
-    if (warning.code !== 'THIS_IS_UNDEFINED') {
-      console.error(`(!) ${warning.message}`);
-    }
+  esm: {
+    // non-minified bundle
+    input: './src/elements/ix-video.ts',
+    output: {
+      dir: './dist/esm/',
+      format: 'esm',
+    },
+    onwarn(warning) {
+      if (warning.code !== 'THIS_IS_UNDEFINED') {
+        console.error(`(!) ${warning.message}`);
+      }
+    },
+    plugins: [
+      replace({
+        'video.js/dist/video.min.js': 'video.js',
+        preventAssignment: true,
+      }),
+      ts(),
+      resolve(),
+      postcss(),
+      postcssLit(),
+      commonjs(),
+      summary(),
+    ],
   },
-  plugins: [
-    ts(),
-    replace({'Reflect.decorate': 'undefined', preventAssignment: true}),
-    resolve(),
-    postcss(),
-    postcssLit(),
-    commonjs(),
-    summary(),
-  ],
 };
+console.log(
+  `Building ix-video ${devMode ? 'development' : 'production'} bundle`
+);
+console.log(`Building video.js ${vjsBundleType} bundle`);
+
+export default buildConfig[vjsBundleType];
