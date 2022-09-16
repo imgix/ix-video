00ce21f1694d205b8a55806e9e9ec95fab7d9088
Luis Ball <lball@imgix.com>
2022-09-16 15:31:02 -0700

    chore: remove committed vuepress dist files

--- docs/src/.vuepress/dist/assets/js/8.f0a1d003.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{568:function(e,t,i){"use strict";i.r(t);var s=i(107),n=Object(s.a)({},(function(){var e=this.$createElement;return(this._self._c||e)("ix-video",{attrs:{controls:"",source:"https://assets.imgix.video/videos/girl-reading-book-in-library.mp4"}})}),[],!1,null,null,null);t.default=n.exports}}]);
