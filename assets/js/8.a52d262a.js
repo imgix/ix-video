a32999deae34ec6ae0b7a19e584dcdea2ebd88a6
Luis Ball <lball@imgix.com>
2022-10-13 12:14:16 -0700

    Updates

---
+++ assets/js/8.a52d262a.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{583:function(e,t,i){"use strict";i.r(t);var s=i(100),n=Object(s.a)({},(function(){var e=this.$createElement;return(this._self._c||e)("ix-video",{attrs:{controls:"",source:"https://assets.imgix.video/videos/girl-reading-book-in-library.mp4"}})}),[],!1,null,null,null);t.default=n.exports}}]);
